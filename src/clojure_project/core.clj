d9b7b5e24f295b89570f526ecb0a629b47b21580
Daniel <danielrcomp@gmail.com>
2018-08-03 09:39:24 -0300

    Function to get the tr content from the movs

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -1,8 +1,9 @@
 (ns clojure-project.core
   (:require [org.httpkit.client :as http]
-            [net.cgrand.enlive-html :as html]))
+            [net.cgrand.enlive-html :as html]
+            [clojure.string :as strs]))
 
-(def url "http://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=E200091IH0000&processo.foro=506&uuidCaptcha=sajcaptcha_9f4bf726470948bba03218716650a93a")
+(def url "https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=1H0008UPE0000&processo.foro=53&uuidCaptcha=sajcaptcha_2739699031fa49a79ba4f010644d55f3")
 
 (defn get-dom
   [url]
@@ -10,13 +11,38 @@
     (:body @(http/get url {:insecure? true}))))
 
 (defn get-movs-info
+  "Get the tbody the containing the content of all movs"
   [content]
   (html/select content [:tbody#tabelaTodasMovimentacoes])
   )
 
+(defn get-trs
+  "Find all trs elements from the mov"
+  [movs-info]
+  (html/select movs-info [:tr])
+  )
+
+
+(defn split-movs [tr-text]
+  "Find the mov date"
+  (first (re-seq #"[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}" tr-text))
+  )
+
+(defn clean [texto]
+  "Clean the text of a mov"
+  (strs/replace texto #"\t|\n" ""))
+
 (defn -main []
 
-  (get-movs-info (get-dom url))
+  (let [movs-content (get-movs-info (get-dom url))
+        trs-content (get-trs movs-content)
+        text-content (vec (map html/text trs-content))
+        cleaned-text (vec  (map clean text-content))
+        ]
+    (println (map split-movs cleaned-text ))
+
+    )
+
 
   )
 
