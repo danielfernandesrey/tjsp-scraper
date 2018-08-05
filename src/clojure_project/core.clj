bc9d183342c72d38a39cd530c3610fb9188568a2
Daniel <danielrcomp@gmail.com>
2018-08-05 15:54:18 -0300

    New functions to get info about the "partes" and "movimentações" of a litigation

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -22,32 +22,66 @@
   (html/select movs-info [:tr])
   )
 
+(defn get-movs-content [tr-text]
+  (strs/trim (get  (strs/split tr-text #"[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4} ") 1))
+  )
 
-(defn split-movs [tr-text]
+(defn get-movs-date [tr-text]
   "Find the mov date"
   (first (re-seq #"[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}" tr-text))
   )
+
+(defn not-blank? [str]
+  (not (strs/blank? str))
+  )
+
 
 (defn clean [texto]
   "Clean the text of a mov"
   (strs/replace texto #"\t|\n" ""))
 
-(defn -main []
+(defn get-partes-info [tr]
+    (let [parte-text (clean (html/text tr))
+          partes-advogados-list (filterv not-blank? (strs/split parte-text #"  " ))
 
-  (let [movs-content (get-movs-info (get-dom url))
-        trs-content (get-trs movs-content)
-        text-content (vec (map html/text trs-content))
-        cleaned-text (vec  (map clean text-content))
-        ]
-    (println (map split-movs cleaned-text ))
-
-    )
-
-
+          ]
+      partes-advogados-list
+      )
   )
 
 
+(defn get-partes [dom]
 
+  (let [tabela (html/select dom [:table#tableTodasPartes])
+        trs (get-trs tabela)
+        partes (map get-partes-info trs)
+        ]
+    partes
+    )
+  )
 
-;(def url "https://www.oantagonista.com/")
+(defn get-movimentacoes [dom]
 
+  (let [movs-content (get-movs-info dom)
+        trs-content (get-trs movs-content)
+        text-content (map html/text trs-content)
+        cleaned-text (map clean text-content)
+        movs-date (vec (map get-movs-date cleaned-text))
+        movs-content (vec (map get-movs-content cleaned-text))
+        lista-movimentacoes (map vector movs-date movs-content)
+        ]
+    lista-movimentacoes
+    )
+  )
+
+(defn -main []
+  (let [dom (get-dom url)
+        partes (get-partes dom )
+        lista-movimentacoes (get-movimentacoes dom)
+
+        ]
+    (hash-map :movimentacoes lista-movimentacoes
+              :partes partes
+              )
+    )
+  )
