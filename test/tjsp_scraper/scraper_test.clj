4e825b0bab012d57a338782c9fec17b03aa4cc23
Daniel <danielrcomp@gmail.com>
2018-08-07 00:16:08 -0300

    Test for the extract-process-info

--- test/tjsp_scraper/scraper_test.clj
+++ test/tjsp_scraper/scraper_test.clj
@@ -5,15 +5,24 @@
 (deftest scraper-test
 
   (testing "get-process-params"
-    (let [numeracao_unica "11277690920168260100"
-          params (get-process-params numeracao_unica)
+    (let [numeracao-unica "11277690920168260100"
+          params (get-process-params numeracao-unica)
           query-params (get params :query-params)
           numero-digito-ano-unificado (get query-params :numeroDigitoAnoUnificado)
           foro-numero-unificado (get query-params :forumNumeroUnificado)
           ]
       (is (instance? clojure.lang.PersistentArrayMap params))
-      (is (= numero-digito-ano-unificado (subs numeracao_unica 0 13)))
-      (is (= foro-numero-unificado (subs numeracao_unica 16)))
+      (is (= numero-digito-ano-unificado (subs numeracao-unica 0 13)))
+      (is (= foro-numero-unificado (subs numeracao-unica 16)))
+      )
+    )
+  (testing "extract-process-info"
+    (let [numeracao-unica "11277690920168260100"
+          process-info (extract-process-info numeracao-unica)
+          ]
+      (is (contains? process-info :dados))
+      (is (contains? process-info :partes))
+      (is (contains? process-info :movimentacoes))
       )
     )
   )
